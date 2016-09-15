a9bfa1c756e98d31aab55083df48833075007da8
Michael Dellapi <mad758@nyu.edu>
2016-09-15 16:48:45 -0400

    first commit, movement and camera scripts

---
+++ Assets/movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	public float cameraX;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(0,0,10*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(0,0,-10f*Time.deltaTime);
+			}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			transform.eulerAngles += new Vector3 (0, 90f, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			transform.eulerAngles += new Vector3 (0, -90f, 0);
+		}
+
+		Camera.main.transform.position=transform.position + new Vector3(0,2f,-3f);
+
+	}
+}
